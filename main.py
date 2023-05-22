34ae085f9fe912b5dceca15a0afc6df59ce30721
Muhammad Taufiqul Mawarid <22143574+taufiqulmawarid@users.noreply.github.com>
2023-05-22 22:55:07 +0700

    gui

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pprint import pformat
 from argparse import ArgumentParser
 from os import makedirs
 import threading
+import tkinter as tk
+import sys
 
 # RUN IN PYTHON 3.8.8
 import server_node
@@ -45,25 +47,79 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     logger.error(f"Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 def main():
-    # parser = ArgumentParser()
-    # parser.add_argument("-N", type=str, dest="node_number",
-    #                     help=" ")
-    # Relay number akan diset di client.py
-    # parser.add_argument("-R", type=str, dest="relay_number",
-    #                     help=" ")
-    # Mungkin server response hardcoded aja
-    # parser.add_argument("-S", type=str, dest="server_response",
-    #                     help=" ")
-    # args = parser.parse_args()
 
-    # logger.info("Processing args...")
-    # node_number: int = int(args.node_number)
-    # logger.info("Done processing args...")
-    node_number = int(input("Node_number: "))
-    execution(node_number)
+    def validate_numeric_input(value: str):
+        if value.isdigit() and value != "0" or value == "":
+            return True
+        return False
+
+    def check_if_all_entry_filled(*args):
+        for each_var in (node_number_var, node_number_in_relay_var, message_to_send_var):
+            # If any entry is still empty disable sim start
+            if each_var.get() == "":
+                sim_start_button.config(state="disabled")
+                return
+        # Else enable sim start
+        sim_start_button.config(state="normal")
+
+    def on_node_number_input_update(*args):
+        if node_number_var.get() == "":
+            node_number_in_relay_input.config(state="disabled")
+        else:
+            node_number_in_relay_input.config(state="normal")
+            if node_number_in_relay_var.get() != "":
+                node_number_in_relay_var.set(min(int(node_number_var.get()), int(node_number_in_relay_var.get())))
+
+    def on_node_relay_input_update(*args):
+        if node_number_in_relay_var.get() != "":
+            node_number_in_relay_var.set(min(int(node_number_var.get()), int(node_number_in_relay_var.get())))
+
+    def start_button_pressed():
+        submitted_node_number = int(node_number_var.get())
+        sim_start_button.destroy()
+        for each_var in (node_number_input, node_number_in_relay_input, message_to_send_input):
+            each_var.config(state="disabled")
+        tk.Label(gui_window, text="Simulasi dijalankan!").pack()
+        execution(submitted_node_number, gui_window)
+
+    # Build the gui window
+    gui_window = tk.Tk()
+    gui_window.title("Onion Routing Simulator")
+    gui_width, gui_height = 300, 235
+    gui_window.geometry(f"{gui_width}x{gui_height}")
+    gui_window.resizable(False, False)
+    numeric_validation = gui_window.register(validate_numeric_input)
+
+    # Input widget
+    tk.Label(gui_window, text="Enter number of nodes available for relay", height=2).pack(anchor="w", padx="10")
+    node_number_var = tk.StringVar()
+    node_number_var.trace_add("write", check_if_all_entry_filled)
+    node_number_var.trace_add("write", on_node_number_input_update)
+    node_number_input = tk.Entry(gui_window, validate="key", validatecommand=(numeric_validation, "%P"), textvariable=node_number_var)
+    node_number_input.pack(fill="x", padx="10")
+    tk.Label(gui_window, text="Recommended around 3-10 node").pack(anchor="w", padx="10")
+
+    tk.Label(gui_window, text="Enter number of relay nodes", height=2).pack(anchor="w", padx="10")
+    node_number_in_relay_var = tk.StringVar()
+    node_number_in_relay_var.trace_add("write", check_if_all_entry_filled)
+    node_number_in_relay_var.trace_add("write", on_node_relay_input_update)
+    node_number_in_relay_input = tk.Entry(gui_window, validate="key", validatecommand=(numeric_validation, "%P"), textvariable=node_number_in_relay_var, state="disabled")
+    node_number_in_relay_input.pack(fill="x", padx="10")
+
+    tk.Label(gui_window, text="Enter message to send", height=2).pack(anchor="w", padx="10")
+    message_to_send_var = tk.StringVar()
+    message_to_send_var.trace_add("write", check_if_all_entry_filled)
+    message_to_send_input = tk.Entry(gui_window, textvariable=message_to_send_var)
+    message_to_send_input.pack(fill="x", padx="10")
+
+    # Start button
+    sim_start_button = tk.Button(gui_window, text="Mulai simulasi!", command=start_button_pressed, state="disabled")
+    sim_start_button.pack(pady=10)
+
+    gui_window.mainloop()
 
 
-def execution(node_number):
+def execution(node_number, main_gui):
     logger = logging.getLogger(__name__)
     sys.excepthook = handle_exception
     makedirs("logs", exist_ok=True)
@@ -71,7 +127,7 @@ def execution(node_number):
     logger.info("The main program is running...")
     logger.info(f"Creating server instance at port {server_port}...")
     logger.info("Start running server nodes...")
-    process = NodeProcess(target=server_node.main, args=(
+    process = NodeProcess(target=server_node.main, daemon=True, args=(
         server_port,
         node_number
     ))
@@ -89,7 +145,7 @@ def execution(node_number):
     node_and_port_dict = dict()
     for node_id in range(node_number):
         this_node_port = node_starting_port + node_id
-        process = NodeProcess(target=relay_node.main, args=(
+        process = NodeProcess(target=relay_node.main, daemon=True, args=(
             node_id,
             port_used_for_node,
             this_node_port,
@@ -108,9 +164,10 @@ def execution(node_number):
     logger.info(f"Creating client instance at port {client_port}...")
     reload_logging_config_node(f"Client.txt")
     # Bisa diganti ke NodeProcess jika diperlukan. Untuk sekarang menggunakan threading agar bisa menggunakan input()
-    thread = threading.Thread(target=client.main, name="Client", args=(
+    thread = threading.Thread(target=client.main, name="Client", daemon=True, args=(
         client_port,
-        node_and_port_dict
+        node_and_port_dict,
+        main_gui
     ))
     thread.start()
 
